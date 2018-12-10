8e74f1d1bff1233b164d5de6e7605839d823e088
anas-1995 <anasalazmeh.95@gmail.com>
2018-12-10 10:19:30 +0200

    test noti

--- common/models/active-chat.js
+++ common/models/active-chat.js
@@ -33,50 +33,21 @@ module.exports = function (Activechat) {
   };
 
   Date.prototype.addHours = function (h) {
-    this.setTime(this.getTime() + (h  * 60 * 1000));
+    this.setTime(this.getTime() + (h * 60 * 1000));
     return this;
   }
 
   Activechat.afterRemote('create', function (context, result, next) {
     Activechat.app.Users
     Activechat.app.models.User.findById(result.firstUser, function (err, firstUser) {
-      if (err) {
-        return next(err);
-      }
-      var message1 = {
-        "app_id": "e8a91e90-a766-4f1b-a47e-e3b3f569dbef",
-        "included_segments ": ["Active Users", "Inactive Users"],
-        "send_after": new Date().addHours(3),
-        "contents": {
-          "ar": "لم يتبقى الوقت الكثير حتى تنتهي محادثتك مع " + firstUser.username + " اسرع و ادخل قبل نفاذ الوقت",
-          "en": "your chat with " + firstUser.username + " is about to expire, hurry up an check it out "
-        },
-        "data": {
-          "chatId": result.chatId
-        },
-        "filters": [{
-          "field": "tag",
-          "key": "user_id",
-          "relation": "=",
-          "value": result.secondUser
-        }],
-        "headings": {
-          "en": "Time is running",
-          "ar": "الوقت يمضي",
-        }
-      }
-      sendNewNotification(message1);
-      Activechat.app.models.User.findById(result.secondUser, function (err, secondUser) {
-        if (err) {
-          return next(err);
-        }
-        var message2 = {
+      if (err == null) {
+        var message1 = {
           "app_id": "e8a91e90-a766-4f1b-a47e-e3b3f569dbef",
           "included_segments ": ["Active Users", "Inactive Users"],
           "send_after": new Date().addHours(3),
           "contents": {
-            "ar": "لم يتبقى الوقت الكثير حتى تنتهي محادثتك مع " + secondUser.username + " اسرع و ادخل قبل نفاذ الوقت",
-            "en": "your chat with " + secondUser.username + " is about to expire, hurry up an check it out "
+            "ar": "لم يتبقى الوقت الكثير حتى تنتهي محادثتك مع " + firstUser.username + " اسرع و ادخل قبل نفاذ الوقت",
+            "en": "your chat with " + firstUser.username + " is about to expire, hurry up an check it out "
           },
           "data": {
             "chatId": result.chatId
@@ -85,14 +56,41 @@ module.exports = function (Activechat) {
             "field": "tag",
             "key": "user_id",
             "relation": "=",
-            "value": result.firstUser
+            "value": result.secondUser
           }],
           "headings": {
             "en": "Time is running",
             "ar": "الوقت يمضي",
           }
         }
-        sendNewNotification(message2);
+        sendNewNotification(message1);
+      }
+      Activechat.app.models.User.findById(result.secondUser, function (err2, secondUser) {
+        if (err2 == null) {
+          var message2 = {
+            "app_id": "e8a91e90-a766-4f1b-a47e-e3b3f569dbef",
+            "included_segments ": ["Active Users", "Inactive Users"],
+            "send_after": new Date().addHours(3),
+            "contents": {
+              "ar": "لم يتبقى الوقت الكثير حتى تنتهي محادثتك مع " + secondUser.username + " اسرع و ادخل قبل نفاذ الوقت",
+              "en": "your chat with " + secondUser.username + " is about to expire, hurry up an check it out "
+            },
+            "data": {
+              "chatId": result.chatId
+            },
+            "filters": [{
+              "field": "tag",
+              "key": "user_id",
+              "relation": "=",
+              "value": result.firstUser
+            }],
+            "headings": {
+              "en": "Time is running",
+              "ar": "الوقت يمضي",
+            }
+          }
+          sendNewNotification(message2);
+        }
         next();
       })
     })
