964d0f89ae558a3ddc2fbcad6ed17a8a843ad7e7
anas-1995 <anasalazmeh.95@gmail.com>
2019-02-13 16:16:56 +0200

    test apple reeipt

--- common/models/product.js
+++ common/models/product.js
@@ -1,5 +1,17 @@
 'use strict';
-
-module.exports = function(Product) {
-
+var appleReceiptVerify = require('node-apple-receipt-verify');
+appleReceiptVerify.config({
+  verbose: true
+});
+module.exports = function (Product) {
+  appleReceiptVerify.validate({
+    receipt: "454654546456"
+  }, function (err, products) {
+    if (err) {
+        console.log("errrrrrrr");
+        
+      return console.error(err);
+    }
+    // ok!
+  });
 };
